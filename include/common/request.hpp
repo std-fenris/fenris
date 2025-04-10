724be635916da9d295a528425d7257234d425457
Parth Baranwal <148996511+P4rth-B@users.noreply.github.com>
2025-04-10 16:42:19 +0530

    Add: request struct. (#15)

--- include/common/request.hpp
+++ include/common/request.hpp
@@ -1,4 +1,7 @@
 // common request related data needed by both server and client
+
+#include <cstdint>
+
 enum class RequestType {
     UPLOAD_FILE,
     READ_FILE,
@@ -9,4 +12,12 @@ enum class RequestType {
     LIST_DIR,
     DELETE_DIR,
     TERMINATE
+};
+
+struct Request {
+    RequestType command;
+    char filename[256];
+    uint32_t ip_addr;
+    uint32_t size_d;
+    uint8_t data[];
 };
